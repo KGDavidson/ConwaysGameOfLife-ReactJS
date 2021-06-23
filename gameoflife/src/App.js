7580084baa1acf75b579832b4f7df9b94b06efe4
KGDavidson <kreatord@gmail.com>
2021-06-23 07:32:16 +0100

    Add pause/erase function

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -10,15 +10,19 @@ const CELL_DEAD_COLOUR = "#FFFFFF"
 const TIMESTEP = 50;
 
 let gameGrid;
+let playing = true;
+let canvas;
+let mouseCounter = 0;
 
 class Cell {
-    constructor(ctx, x, y) {
+    constructor(ctx, x, y, alive, lastChanged) {
         this.ctx = ctx;
         this.x = x;
         this.y = y;
-        this.alive = Math.random() > 0.9;
+        this.alive = alive;
         this.aliveColour = CELL_ALIVE_COLOUR;
         this.deadColour = CELL_DEAD_COLOUR;
+        this.lastChanged = lastChanged;
     }
     draw() {
         this.ctx.translate(this.x, this.y);
@@ -28,6 +32,9 @@ class Cell {
         this.ctx.fillStyle = this.alive ? this.aliveColour : this.deadColour;
         this.ctx.fill();
         this.ctx.translate(-this.x, -this.y);
+    }
+    setAlive(alive) {
+        this.alive = alive;
     }
     checkNeighbours(gameGrid) {
         let aliveNeighbours = -1 * (this.alive ? 1 : 0);
@@ -47,6 +54,10 @@ class Cell {
     }
 }
 
+function getMillis(){
+    return Date.now();
+}
+
 function initialiseGrid(ctx, initXSize, initYSize) {
     let newGameGrid = Array(initXSize);
     for (let x = 0; x < initXSize; x++) {
@@ -54,26 +65,35 @@ function initialiseGrid(ctx, initXSize, initYSize) {
     }
     for (let x = 0; x < newGameGrid.length; x++) {
         for (let y = 0; y < newGameGrid[x].length; y++) {
-            newGameGrid[x][y] = new Cell(ctx, x, y)
+            let alive = false//Math.random() > 0.9;
+            let lastChanged = getMillis();
+            newGameGrid[x][y] = new Cell(ctx, x, y, alive, lastChanged)
         }
     }
     return newGameGrid;
 }
 
+
 function App() {
     const canvasRef = useRef(null);
 
     useEffect(()=>{
-        const canvas = canvasRef.current;
+        canvas = canvasRef.current;
+        canvas.focus(); 
         const ctx = canvas.getContext("2d");
         gameGrid = initialiseGrid(ctx, 80, 80);
 
         const render = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.rect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "#e8e8e8";
+            ctx.fill();
             
             for (let x = 0; x < gameGrid.length; x++) {
                 for (let y = 0; y < gameGrid[x].length; y++) {
-                    gameGrid[x][y].checkNeighbours(gameGrid);
+                    if (playing | mouseDown > 0) {
+                        gameGrid[x][y].checkNeighbours(gameGrid);
+                    }
                     gameGrid[x][y].draw();
                 }
             }
@@ -87,7 +107,74 @@ function App() {
         render();
     }, []);
 
-    return (<canvas id="canvas" ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>);
+    function keyPress(event) {
+        console.log(event.keyCode);
+        switch (event.keyCode) {     
+            case(8):     
+                for (let x = 0; x < gameGrid.length; x++) {
+                    for (let y = 0; y < gameGrid[x].length; y++) {
+                        gameGrid[x][y].alive = false;
+                    }
+                }
+                break
+            case(32):
+                playing = !playing;
+                break
+        }
+    }
+    
+    function getMousePos(event) {
+        var totalOffsetX = 0;
+        var totalOffsetY = 0;
+        var canvasX = 0;
+        var canvasY = 0;
+        var currentElement = canvas;
+    
+        do{
+            totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+            totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+        }
+        while(currentElement = currentElement.offsetParent)
+    
+        canvasX = event.pageX - totalOffsetX;
+        canvasY = event.pageY - totalOffsetY;
+    
+        return {x:canvasX, y:canvasY}
+    }
+
+    function drawCell(event, mouseCounter) {
+        if (mouseCounter > 0) {
+            console.log(event.clientX)
+            console.log((CELL_SIZE * 80))
+            let pos = getMousePos(event);
+            let cellX = Math.floor(pos.x / CELL_SIZE);
+            let cellY = Math.floor(pos.y / CELL_SIZE);
+            console.log(pos.x, pos.y);
+            //console.log(cellX, cellY);
+            //console.log(CANVAS_WIDTH, CANVAS_HEIGHT);
+            try {
+                if (getMillis() - gameGrid[cellX][cellY].lastChanged > 200){
+                    gameGrid[cellX][cellY].setAlive(true);
+                    gameGrid[cellX][cellY].lastChanged = getMillis();
+                }
+            } catch (e) {console.log(e)}
+        }
+    }
+
+    function mouseDown(event) {
+        mouseCounter++;
+        drawCell(event, mouseCounter);
+    }
+
+    function mouseUp(e) {
+        mouseCounter--;
+    }
+
+    function mouseMove(event) {
+        drawCell(event, mouseCounter);
+    }
+
+    return (<canvas tabIndex={-1} id="canvas" onKeyDown={keyPress} onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove} ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>);
 }
 
 export default App;
